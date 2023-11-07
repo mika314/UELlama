53876426c38c053b4ef3e84b1eab7e3bd7d1b4e7
getnamo <getnamo@gmail.com>
2023-11-07 10:19:15 -0800

    c++ no cuda build

--- Includes/build-info.h
+++ Includes/build-info.h
@@ -1,7 +1,9 @@
 #ifndef BUILD_INFO_H
 #define BUILD_INFO_H
 
-#define BUILD_NUMBER 1011
-#define BUILD_COMMIT "dadbed9"
+#define BUILD_NUMBER 1258
+#define BUILD_COMMIT "a40f2b6"
+#define BUILD_COMPILER "unknown"
+#define BUILD_TARGET "unknown"
 
 #endif // BUILD_INFO_H
